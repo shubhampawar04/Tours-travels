1b06715b4c305e0db57277be8331f1c3b8de8813
shubhampawar04 <pawarshubham81668@gmail.com>
2024-08-10 10:44:06 +0530

    all css add

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './app.css'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
+import Main from './Components/Main/Main'
+import Navbar from './Components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <Main/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
